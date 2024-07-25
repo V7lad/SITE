71306caeaa48b65c7cbf12aba15639233e9830e5
V7lad <vladsas2005@mail.ru>
2024-07-25 21:21:26 +0500

    calculator

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ let ask5 = window.document.querySelector('#ask5')
 let ask6 = window.document.querySelector('#ask6')
 let ask7 = window.document.querySelector('#ask7')
 
+let btn1_1 = window.document.querySelector('#calculatorBtn1_1')
+let btn1_2 = window.document.querySelector('#calculatorBtn1_2')
+let btn2_1 = window.document.querySelector('#calculatorBtn2_1')
+let btn2_2 = window.document.querySelector('#calculatorBtn2_2')
+let btn3_1 = window.document.querySelector('#calculatorBtn3_1')
+let btn3_2 = window.document.querySelector('#calculatorBtn3_2')
+let btn4_1 = window.document.querySelector('#calculatorBtn4_1')
+let btn4_2 = window.document.querySelector('#calculatorBtn4_2')
+let btn4_3 = window.document.querySelector('#calculatorBtn4_3')
+let btn4_4 = window.document.querySelector('#calculatorBtn4_4')
+
+let price = window.document.querySelector('#price')
+
+
 const click1 = (event, item) => {
   console.log(item1.style.display)
   console.log(item)
@@ -224,7 +238,7 @@ window.addEventListener('scroll', function (event) { // событие, кото
 
   if (count == 0 && (this.window.innerHeight) / 10 <= viewElementWrite1.top && viewElementWrite1.top <= (this.window.innerHeight)) {
     count = 1
-    WriteText(' Здравствуйте! Меня зовут Сас Владислав, и я являюсь репетитором по математике и компьютерным наукам. Я помогаю своим ученикам улучшить оценки, подготовиться к экзаменам и поступлению в ведущие вузы страны.',"greatingText",0)
+    WriteText(' Здравствуйте! Меня зовут Сас Владислав, и я являюсь репетитором по математике и компьютерным наукам. Я помогаю своим ученикам улучшить оценки, подготовиться к экзаменам и поступлению в ведущие вузы страны.', "greatingText", 0)
 
   }
 
@@ -234,16 +248,188 @@ window.addEventListener('scroll', function (event) { // событие, кото
 
 
 
-function WriteText(text, id, i) {
+function WriteText(text, id, i = 0, speed = 40) {
   console.log(i, text.charAt(i))
   let txt = String(text); /* Текст */
-  const speed = 40;/* Скорость/длительность эффекта в миллисекундах */
   if (i < txt.length) {
     document.getElementById(String(id)).innerHTML += txt.charAt(i);
     i++;
-    setTimeout(WriteText, speed,text, id, i); //функция,скорость,аргументы в функцию
+    setTimeout(WriteText, speed, text, id, i); //функция,скорость,аргументы в функцию
   }
 }
+
+let b1 = 0;
+let b2 = 0;
+let b3 = 0;
+let b4 = 0;
+
+function clickCalculatorBtn(id) {
+  if (id == 11) {
+    if (btn1_2.style.visibility == 'hidden') {
+      btn1_2.style.visibility = 'visible';
+      btn1_1.style.position = 'relative';
+      b1 = 0;
+    }
+    else {
+      b1 = 1
+      btn1_2.style.visibility = 'hidden';
+      btn1_1.style.position = 'absolute';
+      b1 = 1;
+    }
+  }
+  if (id == 12) {
+    if (btn1_1.style.visibility == 'hidden') {
+      btn1_1.style.visibility = 'visible';
+      btn1_2.style.position = 'relative';
+      b1 = 0;
+    }
+    else {
+      btn1_1.style.visibility = 'hidden';
+      btn1_2.style.position = 'absolute';
+      b1 = 2;
+    }
+  }
+  if (id == 21) {
+    if (btn2_2.style.visibility == 'hidden') {
+      btn2_2.style.visibility = 'visible';
+      btn2_1.style.position = 'relative';
+      b2 = 0;
+    }
+    else {
+      btn2_2.style.visibility = 'hidden';
+      btn2_1.style.position = 'absolute';
+      b2 = 1;
+    }
+  }
+  if (id == 22) {
+    if (btn2_1.style.visibility == 'hidden') {
+      btn2_1.style.visibility = 'visible';
+      btn2_2.style.position = 'relative';
+      b2 = 0;
+    }
+    else {
+      btn2_1.style.visibility = 'hidden';
+      btn2_2.style.position = 'absolute';
+      b2 = 2;
+    }
+  }
+  if (id == 31) {
+    if (btn3_2.style.visibility == 'hidden') {
+      btn3_2.style.visibility = 'visible';
+      btn3_1.style.position = 'relative';
+      b3 = 0;
+    }
+    else {
+      btn3_2.style.visibility = 'hidden';
+      btn3_1.style.position = 'absolute';
+      b3 = 1;
+    }
+  }
+  if (id == 32) {
+    if (btn3_1.style.visibility == 'hidden') {
+      btn3_1.style.visibility = 'visible';
+      btn3_2.style.position = 'relative';
+      b3 = 0;
+    }
+    else {
+      btn3_1.style.visibility = 'hidden';
+      btn3_2.style.position = 'absolute';
+      b3 = 2;
+    }
+  }
+  if (id == 41) {
+    if (btn4_2.style.visibility == 'hidden') {
+      btn4_2.style.visibility = 'visible';
+      btn4_3.style.visibility = 'visible';
+      btn4_4.style.visibility = 'visible';
+      btn4_2.style.position = 'relative';
+      btn4_3.style.position = 'relative';
+      btn4_4.style.position = 'relative';
+      b4 = 0;
+    }
+    else {
+      btn4_2.style.visibility = 'hidden';
+      btn4_3.style.visibility = 'hidden';
+      btn4_4.style.visibility = 'hidden';
+      btn4_2.style.position = 'absolute';
+      btn4_3.style.position = 'absolute';
+      btn4_4.style.position = 'absolute';
+      b4 = 1;
+    }
+  }
+  if (id == 42) {
+    if (btn4_1.style.visibility == 'hidden') {
+      btn4_1.style.visibility = 'visible';
+      btn4_3.style.visibility = 'visible';
+      btn4_4.style.visibility = 'visible';
+      btn4_1.style.position = 'relative';
+      btn4_3.style.position = 'relative';
+      btn4_4.style.position = 'relative';
+      b4 = 0;
+
+    }
+    else {
+      btn4_1.style.visibility = 'hidden';
+      btn4_3.style.visibility = 'hidden';
+      btn4_4.style.visibility = 'hidden';
+      btn4_1.style.position = 'absolute';
+      btn4_3.style.position = 'absolute';
+      btn4_4.style.position = 'absolute';
+      b4 = 2;
+    }
+  }
+  if (id == 43) {
+    if (btn4_2.style.visibility == 'hidden') {
+      btn4_2.style.visibility = 'visible';
+      btn4_1.style.visibility = 'visible';
+      btn4_4.style.visibility = 'visible';
+      btn4_1.style.position = 'relative';
+      btn4_2.style.position = 'relative';
+      btn4_4.style.position = 'relative';
+      b4 = 0;
+
+    }
+    else {
+      btn4_2.style.visibility = 'hidden';
+      btn4_1.style.visibility = 'hidden';
+      btn4_4.style.visibility = 'hidden';
+      btn4_1.style.position = 'absolute';
+      btn4_2.style.position = 'absolute';
+      btn4_4.style.position = 'absolute';
+      b4 = 3;
+    }
+  }
+  if (id == 44) {
+    if (btn4_2.style.visibility == 'hidden') {
+      btn4_2.style.visibility = 'visible';
+      btn4_3.style.visibility = 'visible';
+      btn4_1.style.visibility = 'visible';
+      btn4_2.style.position = 'relative';
+      btn4_3.style.position = 'relative';
+      btn4_1.style.position = 'relative';
+      b4 = 0;
+
+    }
+    else {
+      btn4_2.style.visibility = 'hidden';
+      btn4_3.style.visibility = 'hidden';
+      btn4_1.style.visibility = 'hidden';
+      btn4_2.style.position = 'absolute';
+      btn4_3.style.position = 'absolute';
+      btn4_1.style.position = 'absolute';
+      b4 = 4;
+    }
+
+  }
+  console.log(b1, b2, b3, b4)
+  if (b1 != 0 && b2 != 0 && b3 != 0 && b4 != 0) {
+    WriteText('1000 rub', 'price', 0,1)
+  }
+  else{
+    price.innerHTML='';
+  }
+}
+
 
 
 // var i = 0;
